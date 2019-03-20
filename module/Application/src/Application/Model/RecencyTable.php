1b1022819de1e9520618371645fd1e0a4a1ac16e
Amit Dugar <amit@deforay.com>
2019-03-20 23:57:08 +0530

    Result PDF updated with new fields. Email Result issue fixed.

--- module/Application/src/Application/Model/RecencyTable.php
+++ module/Application/src/Application/Model/RecencyTable.php
@@ -42,7 +42,7 @@ class RecencyTable extends AbstractTableGateway {
 
 
                          
-          $aColumns = array('r.sample_id','r.patient_id','DATE_FORMAT(r.sample_collection_date,"%d-%b-%Y"','DATE_FORMAT(r.sample_receipt_date,"%d-%b-%Y"','r.received_specimen_type','f.facility_name' ,'ft.facility_name' ,'DATE_FORMAT(r.hiv_diagnosis_date,"%d-%b-%Y")','DATE_FORMAT(r.hiv_recency_date,"%d-%b-%Y")','r.control_line','r.positive_verification_line','r.long_term_verification_line','r.kit_lot_no','DATE_FORMAT(r.kit_expiry_date,"%d-%b-%Y")','r.term_outcome','r.final_outcome','r.vl_result','r.tester_name','DATE_FORMAT(r.dob,"%d-%b-%Y")','r.age','r.gender','r.marital_status','r.residence','r.education_level','rp.name','r.pregnancy_status','r.current_sexual_partner','r.past_hiv_testing','r.last_hiv_status','r.patient_on_art','r.test_last_12_month','r.exp_violence_last_12_month','DATE_FORMAT(r.form_initiation_datetime ,"%d-%b-%Y %H:%i:%s")','DATE_FORMAT(r.form_transfer_datetime ,"%d-%b-%Y %H:%i:%s")','DATE_FORMAT(r.vl_test_date,"%d-%b-%Y")');
+          $aColumns = array('r.sample_id','r.patient_id','DATE_FORMAT(r.sample_collection_date,"%d-%b-%Y")','DATE_FORMAT(r.sample_receipt_date,"%d-%b-%Y")','r.received_specimen_type','f.facility_name' ,'ft.facility_name' ,'DATE_FORMAT(r.hiv_diagnosis_date,"%d-%b-%Y")','DATE_FORMAT(r.hiv_recency_date,"%d-%b-%Y")','r.control_line','r.positive_verification_line','r.long_term_verification_line','r.kit_lot_no','DATE_FORMAT(r.kit_expiry_date,"%d-%b-%Y")','r.term_outcome','r.final_outcome','r.vl_result','r.tester_name','DATE_FORMAT(r.dob,"%d-%b-%Y")','r.age','r.gender','r.marital_status','r.residence','r.education_level','rp.name','r.pregnancy_status','r.current_sexual_partner','r.past_hiv_testing','r.last_hiv_status','r.patient_on_art','r.test_last_12_month','r.exp_violence_last_12_month','DATE_FORMAT(r.form_initiation_datetime ,"%d-%b-%Y %H:%i:%s")','DATE_FORMAT(r.form_transfer_datetime ,"%d-%b-%Y %H:%i:%s")','DATE_FORMAT(r.vl_test_date,"%d-%b-%Y")');
           $orderColumns = array('r.sample_id','r.patient_id','r.sample_collection_date','r.sample_receipt_date','r.received_specimen_type','f.facility_name','f.facility_name','ft.facility_name', 'r.hiv_diagnosis_date','r.hiv_recency_date','r.control_line','r.positive_verification_line','r.long_term_verification_line','r.kit_lot_no','r.kit_expiry_date','r.term_outcome','r.final_outcome','r.vl_result','r.tester_name','r.dob','r.age','r.gender','r.marital_status','r.residence','r.education_level','rp.name','r.pregnancy_status','r.current_sexual_partner','r.past_hiv_testing','r.last_hiv_status','r.patient_on_art','r.test_last_12_month','r.exp_violence_last_12_month','r.form_initiation_datetime','r.form_transfer_datetime','r.vl_test_date');
 
           /* Paging */
@@ -1766,14 +1766,17 @@ return $rResult;
         {
             $tempDb = new \Application\Model\TempMailTable($this->adapter);
 
+            $config = new \Zend\Config\Reader\Ini();
+            $configResult = $config->fromFile(CONFIG_PATH . '/custom.config.ini');            
+
             $emailFormField = json_decode($params['emailResultFields'],true);
             $to = $emailFormField['toEmail'];
             $subject = $emailFormField['subject'];
             $message = $emailFormField['message'];
-            $fromName = 'Admin';
+            $fromName = 'HIV Recency Testing';
             $attachment = $params['pdfFile'];
-            $fromMail  = 'zfmailexample@gmail.com';
-            $mailResult = '';
+            $fromMail  = $configResult["email"]["config"]["username"];
+            $mailResult = 0;
             $mailResult = $tempDb->insertTempMailDetails($to, $subject, $message, $fromMail, $fromName,$cc=null,$bcc=null,$attachment);
             if($mailResult>0)
             {
