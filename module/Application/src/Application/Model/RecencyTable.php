19f170d7d7bf943960878d229eea77354f0616c1
Thanaseelan <thanaseelan@deforay.com>
2019-08-21 17:18:11 +0530

    Few changes done

--- module/Application/src/Application/Model/RecencyTable.php
+++ module/Application/src/Application/Model/RecencyTable.php
@@ -4274,7 +4274,7 @@ class RecencyTable extends AbstractTableGateway
     public function fetchKitInfo($kitNo){
         $dbAdapter = $this->adapter;
         $sql = new Sql($dbAdapter);
-        $sQuery = $sql->select()->from('test_kit_information')->columns(array('test_id','kit_lot_no','kit_expiry_date' => new Expression("DATE_FORMAT(kit_expiry_date,'%d-%b-%Y')")));
+        $sQuery = $sql->select()->from('test_kit_information')->columns(array('test_id','kit_lot_no','kit_expiry_date' => new Expression("DATE_FORMAT(kit_expiry_date,'%d-%b-%Y')")))->where(array('status'=>'active'));
         if(isset($kitNo) && $kitNo != 0){
             $sQuery= $sQuery->where(array('kit_lot_no'=>$kitNo));
         }
